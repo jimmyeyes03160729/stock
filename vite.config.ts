f5d249c95ede12a22553c00f18538b838f0fff70
jimmyeyes03160729 <jimmyeyes0316@gmail.com>
2026-09-06 22:44:34 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  // 特別設定：對應倉庫名稱 /stock/，防止打包後資源 404
+  base: '/stock/',
+});
